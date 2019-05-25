5b26c471d89bdd8dff14f0f69a631867038d0386
Sandra Bigyak <sandra.bidiak@gmail.com>
2019-05-25 19:41:14 +0200

    - added routes

---
+++ bitsfabrick/src/components/PlotList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PlotList() {
+    return (
+      <div>
+       <Link to="detail">
+            Create Plot List 
+       </Link>
+      </div>
+    );
+  }
+  
+  export default PlotList;
